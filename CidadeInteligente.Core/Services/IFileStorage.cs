96a86b8abe8d556a9b335678c7e56edb248726f7
MarioGuilherme <marioguilhermedev@gmail.com>
2024-09-20 22:16:58 -0300

    style: nome da variável na interface

--- CidadeInteligente.Core/Services/IFileStorage.cs
+++ CidadeInteligente.Core/Services/IFileStorage.cs
@@ -2,5 +2,5 @@
 
 public interface IFileStorage {
     Task DeleteFileAsync(string fileName);
-    Task<string> UploadOrUpdateFileAsync(string extension, byte[] bytes);
+    Task<string> UploadOrUpdateFileAsync(string fileName, byte[] bytes);
 }
